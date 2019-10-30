c9974673b2f0f30d065981d27b9086fa170d76e3
James Read <james.read.18@gmail.com>
2019-10-30 16:08:34 +0000

    fixed issue with basic account, nicely formatted errors (#11)
    * fixed issue with basic account, nicely formmated errors
    
    * chore(release): 1.4.13
    
    * chore(coverage): remove coverage form npm repo

--- src/services/submission.ts
+++ src/services/submission.ts
@@ -21,6 +21,7 @@ import {
 import { Service } from './service';
 
 export class Submission extends Service {
+
   private options = {
     headless: true,
   } as OptionsV2; // not the correct build options
@@ -30,40 +31,51 @@ export class Submission extends Service {
     this.log('createFullCase', fullCase);
 
     return Promise.resolve(fullCase)
-      .then(caseObj =>
-        this.postRequest(
-          builder.buildObject(
-            new CaseTransformer(this.credentials).item(caseObj),
-          ),
+      .then(caseObj => {
+        return this.postRequest(
+          builder.buildObject(new CaseTransformer(this.credentials).item(caseObj)),
           'url',
           'api/CreateFullCaseWithReturn',
-        ),
-      )
-      .then(caseResult =>
-        new CaseResultTransformer(this.credentials).xmlItem(caseResult),
-      )
-      .catch(e => {
-        this.log('createFullCase', e);
-
-        throw new PartnerLinkError(
-          !e.code ? `Unable to submit case (${e.message}).` : e.message,
-          !e.code ? 406 : e.code,
         );
+      })
+      .then(caseResult => new CaseResultTransformer(this.credentials).xmlItem(caseResult))
+      .catch(e => {
+        this.log('createFullCase: catch', e);
+
+        // already been caught
+        if (e instanceof PartnerLinkError) {
+          throw e;
+        }
+
+        return this.parseXml(e.error)
+          .then(errObj => {
+            if (errObj.hasOwnProperty('string')) {
+              // bad auth
+              throw new PartnerLinkError(
+                `Unable to submit case (${JSON.parse(errObj.string._).error_description}).`,
+                406,
+              );
+            }
+
+            const array = Object.keys(errObj)[0];
+            const item = Object.keys(errObj[array])[1];
+            const textError = errObj[array][item].map(i => {
+              return i.Value[0]['d3p1:_errors'][0]['d3p1:ModelError'][0]['d3p1:_x003C_ErrorMessage_x003E_k__BackingField'][0];
+            });
+
+            // parsed error response
+            throw new PartnerLinkError(`Unable to submit case (${textError.join('\n')}).`, 406);
+          });
       });
   }
 
-  public addAddresses(
-    caseInformation: CaseResult,
-    addresses: CreditSearchAddressResult[],
-  ): Promise<CaseResult> {
+  public addAddresses(caseInformation: CaseResult, addresses: CreditSearchAddressResult[]): Promise<CaseResult> {
     this.log('addAddresses', caseInformation, addresses);
     const builder = new xml2js.Builder(this.options);
     return Promise.resolve({ info: caseInformation, addresses })
       .then(({ info, addresses }) => {
         return this.postRequest(
-          builder.buildObject(
-            new AddAddressTransformer(this.credentials).items(addresses),
-          ),
+          builder.buildObject(new AddAddressTransformer(this.credentials).items(addresses)),
           'url',
           'api/AddAddresses/' + info.id,
         ).then(resp => {
@@ -80,10 +92,7 @@ export class Submission extends Service {
       });
   }
 
-  public addNotes(
-    caseInformation: CaseResult,
-    notes: NoteRequest,
-  ): Promise<CaseResult> {
+  public addNotes(caseInformation: CaseResult, notes: NoteRequest): Promise<CaseResult> {
     this.log('addNotes', caseInformation, notes);
     const builder = new xml2js.Builder(this.options);
     return Promise.resolve({ info: caseInformation, notes })
@@ -106,10 +115,7 @@ export class Submission extends Service {
       });
   }
 
-  public addCreditors(
-    caseInformation: CaseResult,
-    creditors: Creditor[],
-  ): Promise<CaseResult> {
+  public addCreditors(caseInformation: CaseResult, creditors: Creditor[]): Promise<CaseResult> {
     this.log('addCreditors', caseInformation, creditors);
 
     const builder = new xml2js.Builder(this.options);
@@ -123,40 +129,35 @@ export class Submission extends Service {
           'api/AddCreditors/' + info.id,
         ),
       )
-      .then(results => caseInformation)
+      .then(() => caseInformation)
       .catch(e => {
-        this.log('addCreditors', e);
-        throw new PartnerLinkError(
-          !e.code ? `Unable to submit creditors (${e.message}).` : e.message,
-          !e.code ? 406 : e.code,
-        );
+        this.log('addCreditors: catch', e);
+        return this.parseXml(e.error)
+          .then(errObj => this.parseXml(errObj.string._))
+          .then(errObj => {
+            throw new PartnerLinkError(
+              `Unable to submit creditors (${errObj.string._}).`,
+              406,
+            );
+          });
       });
   }
 
-  public addCreditor(
-    caseInformation: CaseResult,
-    creditor: Creditor,
-  ): Promise<CaseResult> {
+  public addCreditor(caseInformation: CaseResult, creditor: Creditor): Promise<CaseResult> {
     this.log('addCreditor', caseInformation, creditor);
     return this.addCreditors(caseInformation, [creditor])
-      .then(results => caseInformation)
+      .then(() => caseInformation)
       .catch(e => {
-        this.log('addCreditor', e);
+        this.log('addCreditor: catch', e);
+
         throw new PartnerLinkError(
-          !e.code
-            ? `Unable to submit creditor ${creditor.name} for ${
-                creditor.currentBalance
-              }.`
-            : e.message,
+          !e.code ? `Unable to submit creditor ${creditor.name} for ${creditor.currentBalance}.` : e.message,
           !e.code ? 406 : e.code,
         );
       });
   }
 
-  public addDocuments(
-    caseInformation: CaseResult,
-    documents: Document[],
-  ): Promise<CaseResult> {
+  public addDocuments(caseInformation: CaseResult, documents: Document[]): Promise<CaseResult> {
     this.log('addDocuments', caseInformation, documents);
 
     return Promise.all(
@@ -164,7 +165,7 @@ export class Submission extends Service {
     )
       .then(results => caseInformation)
       .catch(e => {
-        this.log('addDocuments', e);
+        this.log('addDocuments: catch', e);
         const err = new PartnerLinkError(
           !e.code ? `Unable to submit documents (${e.message}).` : e.message,
           !e.code ? 406 : e.code,
@@ -174,16 +175,14 @@ export class Submission extends Service {
       });
   }
 
-  public addDocument(
-    caseInformation: CaseResult,
-    document: Document,
-  ): Promise<CaseResult> {
+  public addDocument(caseInformation: CaseResult, document: Document): Promise<CaseResult> {
+
     this.log('addDocument', caseInformation, document);
 
     const builder = new xml2js.Builder(this.options);
     return Promise.resolve({ info: caseInformation, document })
-      .then(({ info, document }) =>
-        this.tokenPostRequest(
+      .then(({ info, document }) => {
+        return this.tokenPostRequest(
           builder.buildObject(
             new DocumentTransformer().item({
               id: info.id,
@@ -192,16 +191,14 @@ export class Submission extends Service {
           ),
           'url',
           'api/AddDocument',
-        ),
-      )
-      .then(results => caseInformation)
+        );
+      })
+      .then(() => caseInformation)
       .catch(e => {
-        this.log('addDocument', e);
+        this.log('addDocument: catch', e);
 
         throw new PartnerLinkError(
-          !e.code
-            ? `Unable to submit document ${document.fileName}.`
-            : e.message,
+          !e.code ? `Unable to submit document ${document.fileName}.` : e.message,
           !e.code ? 406 : e.code,
         );
       });
